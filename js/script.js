6f5320c9385ee391e8b8cfbf086aefac5e292189
Andrezza Souza <andrezzasouza@id.uff.br>
2021-08-16 03:51:28 -0300

    Avançou com a versão mobile

--- js/script.js
+++ js/script.js
@@ -737,7 +737,13 @@ function renderFinalizedQuizz(response) {
   document.querySelector(".screen11").innerHTML = innerFinalizedScreen;
   document.querySelector(
     ".created-quizz-img"
-  ).style.backgroundImage = `url("${quizzUrlInput}")`;
+  ).style.background = `background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0) 0%,
+    rgba(0, 0, 0, 0.5) 64.58%,
+    #000000 100%
+  ),
+  url("${quizzUrlInput}")`;
 }
 
 function toFinalizeQuizz(response) {
